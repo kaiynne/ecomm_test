14cfc361bc2a3b7d0458e3b426f505d85cacb430
Kain Warwick <kain.warwick@gmail.com>
2012-05-04 10:11:16 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcommTest::Application.config.secret_token = 'e643d3aeb8c0d80394adbf7f6ed63852623181d10ba0a359f1dae73464cf19bcf8937a552bc2e598e158151a766f2e02b162a24c347293e1fc6b0ce0b2f6896e'
